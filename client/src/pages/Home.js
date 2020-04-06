275e9d59a2f16478a9efb47e4c82832c0cbae703
Fred Guthrie <fred3@myself.com>
2020-04-06 13:34:53 -0500

    doh2

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,61 +16,70 @@ class TPComponent extends Component {
             Amzitems: [],
             WalProd: [],
             WalPrice: [],
-            WalAval: []
+            WalAval: [],
+            AmzProd: [],
+            AmzPrice: [],
+            AmzTital: []
         }
     }
     componentDidMount() {
 
-        axios({
-            "method": "GET",
-            "url": "https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-search-by-keyword",
-            "headers": {
-                "content-type": "application/octet-stream",
-                "x-rapidapi-host": "axesso-walmart-data-service.p.rapidapi.com",
-                "x-rapidapi-key": "3b8ee98b70mshf74d3fe848bde7dp1f7b3ajsn392bc6e2ea65"
-            }, "params": {
-                "sortBy": "best_match",
-                "page": 1,
-                "keyword": "toilet paper",
-                "type": "text",
-            }
-        })
-            .then((response) => {
-                this.setState({ WalItems: "https://www.walmart.com" + response.data.foundProducts.slice(0, 3) });
-                console.log(response.data.foundProducts.slice(0, 3));
-                WalCall();
-            })
-            .catch((error) => {
-                console.log('error', error)
-            })
-        const WalCall=()=> {
-            console.log(this.state.WalItems);
-            
-            // this.state.WalItems.forEach(item =>
-            //     axios({
-            //         "method": "GET",
-            //         "url": "https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-lookup-product",
-            //         "headers": {
-            //             "content-type": "application/octet-stream",
-            //             "x-rapidapi-host": "axesso-walmart-data-service.p.rapidapi.com",
-            //             "x-rapidapi-key": "3b8ee98b70mshf74d3fe848bde7dp1f7b3ajsn392bc6e2ea65"
-            //         }, "params": {
-            //             "url": item
-            //         }
-            //     })
-            //         .then((response) => {
-            //             this.setState({ WalProd: [...this.state.WalProd, response.data.productTitle] });
-            //             this.setState({ WalPrice: [...this.state.WalPrice, response.data.price] });
-            //             this.setState({ WalAval: [...this.state.WalAval, response.data.available] });
-            //             console.log('WalProd:' + this.state.WalProd[0]);
-            //             console.log('WalPrice' + this.state.WalPrice[0]);
-            //             console.log('WalAval' + this.state.WalAval[0]);
-            //         })
-            //         .catch((error) => {
-            //             console.log('error', error)
-            //         })
-            // )
-        };
+        // axios({
+        //     "method": "GET",
+        //     "url": "https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-search-by-keyword",
+        //     "headers": {
+        //         "content-type": "application/octet-stream",
+        //         "x-rapidapi-host": "axesso-walmart-data-service.p.rapidapi.com",
+        //         "x-rapidapi-key": "3b8ee98b70mshf74d3fe848bde7dp1f7b3ajsn392bc6e2ea65"
+        //     }, "params": {
+        //         "sortBy": "best_match",
+        //         "page": 1,
+        //         "keyword": "toilet paper",
+        //         "type": "text",
+        //     }
+        // })
+        //     .then((response) => {
+        //         this.setState({ WalItems:  response.data.foundProducts.slice(0, 3) });
+        //         console.log(response.data.foundProducts.slice(0, 3));
+        //         WalCall();
+        //     })
+        //     .catch((error) => {
+        //         console.log('error', error)
+        //     })
+        // const WalCall = () => {
+        //     console.log(this.state.WalItems);
+
+        //     this.state.WalItems.forEach(item =>
+        //         axios({
+        //             "method": "GET",
+        //             "url": "https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-lookup-product",
+        //             "headers": {
+        //                 "content-type": "application/octet-stream",
+        //                 "x-rapidapi-host": "axesso-walmart-data-service.p.rapidapi.com",
+        //                 "x-rapidapi-key": "3b8ee98b70mshf74d3fe848bde7dp1f7b3ajsn392bc6e2ea65"
+        //             }, "params": {
+        //                 "url": "https://www.walmart.com" + item
+        //             }
+        //         })
+        //             .then((response) => {
+        //                 this.setState({ WalProd: [...this.state.WalProd, response.data.productTitle] });
+        //                 this.setState({ WalPrice: [...this.state.WalPrice, response.data.price] });
+        //                 this.setState({ WalAval: [...this.state.WalAval, response.data.available] });
+        //                 console.log('WalProd:' + this.state.WalProd[0]);
+        //                 console.log('WalPrice' + this.state.WalPrice[0]);
+        //                 console.log('WalAval' + this.state.WalAval[0]);
+        //                 console.log('WalProd:' + this.state.WalProd[1]);
+        //                 console.log('WalPrice' + this.state.WalPrice[1]);
+        //                 console.log('WalAval' + this.state.WalAval[1]);
+        //                 console.log('WalProd:' + this.state.WalProd[3]);
+        //                 console.log('WalPrice' + this.state.WalPrice[3]);
+        //                 console.log('WalAval' + this.state.WalAval[3]);
+        //             })
+        //             .catch((error) => {
+        //                 console.log('error', error)
+        //             })
+        //     )
+        // };
         /************************************************************************************************** */
         /***************************************************Kmart */
         // axios({
@@ -102,33 +111,61 @@ class TPComponent extends Component {
 
 
 
-        // axios({
-        //   "method":"GET",
-        //   "url":"https://amazon-price1.p.rapidapi.com/search",
-        //   "headers":{
-        //   "content-type":"application/octet-stream",
-        //   "x-rapidapi-host":"amazon-price1.p.rapidapi.com",
-        //   "x-rapidapi-key":"9429490917msh70be5ef070eec0ep1fec84jsnafca7d833618"
-        //   },"params":{
-        //   "keywords":"toilet paper",
-        //   "marketplace":"US"
-        //   }
-        //   })
-        //   .then((response)=>{
-        //     console.log(response)
-        //   })
-        //   .catch((error)=>{
-        //     console.log(error)
-        //   })
-
-        // Amazon api call above
-
-    }
+        axios({
+            "method": "GET",
+            "url": "https://amazon-price1.p.rapidapi.com/search",
+            "headers": {
+                "content-type": "application/octet-stream",
+                "x-rapidapi-host": "amazon-price1.p.rapidapi.com",
+                "x-rapidapi-key": "9429490917msh70be5ef070eec0ep1fec84jsnafca7d833618"
+            }, "params": {
+                "keywords": "toilet paper",
+                "marketplace": "US"
+            }
+        })
+            .then((response) => {
+                // this.setState({ WalItems:  response.data.foundProducts.slice(0, 3) });
+                this.setState({ AmzProd: response.data[0].ASIN });
+                this.setState({ AmzTital: response.data[0].title });
+                this.setState({ AmzProd: response.data[1].ASIN });
+                this.setState({ AmzTital: response.data[1].title });
+                this.setState({ AmzProd: response.data[2].ASIN });
+                this.setState({ AmzTital: response.data[2].title });
+                AmzCall();
+                console.log(response)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        const AmzCall = () => {
+            console.log(this.state.AmzProd);
+            this.state.AmzProd.forEach(item =>
+                axios({
+                    "method": "GET",
+                    "url": "https://amazon-price1.p.rapidapi.com/priceReport",
+                    "headers": {
+                        "content-type": "application/octet-stream",
+                        "x-rapidapi-host": "amazon-price1.p.rapidapi.com",
+                        "x-rapidapi-key": "9429490917msh70be5ef070eec0ep1fec84jsnafca7d833618"
+                    }, "params": {
+                        "asin": item,
+                        "marketplace": "US"
+                    }
+                })
+                    .then((response) => {
+                        console.log(response)
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            )
+        }
+    };
 
     render() {
         return (
             <div>
-                <Nav />
+                {/* <Nav /> */}
                 <section className="tp-banner">
                     <h2><img src={Logo} style={{ height: "110px", width: "150px" }} alt="logo" />TP-Express</h2>
                     <h6 className="brand-slogan">We can find you your TP!</h6>
@@ -141,7 +178,7 @@ class TPComponent extends Component {
                     <div className="card" style={{ width: "18rem" }}>
                         <img className="card-img-top" src={Logo} alt="Card logo cap" />
                         <div className="card-body">
-                            <h5 className="card-title">fred{this.state.WalProd[0]}</h5>
+                            <h5 className="card-title">{this.state.WalProd[0]}</h5>
                             <p className="card-text">Walmart</p>
                         </div>
                         <ul className="list-group list-group-flush">
@@ -159,11 +196,11 @@ class TPComponent extends Component {
                     <div className="card" style={{ width: "18rem" }}>
                         <img className="card-img-top" src={Logo} alt="Card logo cap" />
                         <div className="card-body">
-                            <h5 className="card-title">fred{this.state.WalProd[2]}</h5>
+                            <h5 className="card-title">{this.state.WalProd[1]}</h5>
                             <p className="card-text">Walmart</p>
                         </div>
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Price: {this.state.WalPrice[2]}</li>
+                            <li className="list-group-item">Price: {this.state.WalPrice[1]}</li>
                             {/* <li className="list-group-item">Price From Target</li> */}
                             {/* <li className="list-group-item">Price From Amazon</li> */}
                         </ul>
@@ -176,11 +213,11 @@ class TPComponent extends Component {
                     <div className="card" style={{ width: "18rem" }}>
                         <img className="card-img-top" src={Logo} alt="Card logo cap" />
                         <div className="card-body">
-                            <h5 className="card-title">fred{this.state.WalProd[3]}</h5>
+                            <h5 className="card-title">{this.state.WalProd[2]}</h5>
                             <p className="card-text">Walmart</p>
                         </div>
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Price: {this.state.WalPrice[3]}</li>
+                            <li className="list-group-item">Price: {this.state.WalPrice[2]}</li>
                             {/* <li className="list-group-item">Price From Target</li> */}
                             {/* <li className="list-group-item">Price From Amazon</li> */}
                         </ul>
@@ -203,8 +240,8 @@ class TPComponent extends Component {
                             <li className="list-group-item">Price From Amazon</li>
                         </ul>
                         <div className="card-body">
-                            <a href={{ Product }} className="card-link">View Item</a>
-                            <a href={{ Account }} className="card-link">Add to wishlist</a>
+                            {/* <a href={{ Product }} className="card-link">View Item</a> */}
+                            {/* <a href={{ Account }} className="card-link">Add to wishlist</a> */}
                         </div>
                     </div>
                 </span>
@@ -221,14 +258,14 @@ class TPComponent extends Component {
                             <li className="list-group-item">Price From Amazon</li>
                         </ul>
                         <div className="card-body">
-                            <a href={{ Product }} className="card-link">View Item</a>
-                            <a href={{ Account }} className="card-link">Add to wishlist</a>
+                            {/* <a href={{ Product }} className="card-link">View Item</a> */}
+                            {/* <a href={{ Account }} className="card-link">Add to wishlist</a> */}
                         </div>
                     </div>
                 </span>
             </div>
         );
     };
-    
+
 }
 export default TPComponent;
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -24,85 +24,6 @@ class TPComponent extends Component {
     }
   }
   componentDidMount() {
-<<<<<<< HEAD
-
-    // axios({
-    //   "method": "GET",
-    //   "url": "https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-search-by-keyword",
-    //   "headers": {
-    //     "content-type": "application/octet-stream",
-    //     "x-rapidapi-host": "axesso-walmart-data-service.p.rapidapi.com",
-    //     "x-rapidapi-key": "3b8ee98b70mshf74d3fe848bde7dp1f7b3ajsn392bc6e2ea65"
-    //   }, "params": {
-    //     "sortBy": "best_match",
-    //     "page": 1,
-    //     "keyword": "toilet paper",
-    //     "type": "text",
-    //   }
-    // })
-    //   .then((response) => {
-    //     this.setState({ WalItems: "https://www.walmart.com" + response.data.foundProducts.slice(0, 3) });
-    //     // Item[0]="https://www.walmart.com"+response.data.foundProducts[0];
-    //     // Item[1]="https://www.walmart.com"+response.data.foundProducts[1];
-    //     // Item[2]="https://www.walmart.com"+response.data.foundProducts[2];
-
-
-    //     // console.log('response=',  WalItems[0])
-    //     // console.log('response=',  WalItems[1])
-    //     // console.log('response=',  WalItems[2])
-
-    //   })
-    //   .catch((error) => {
-    //     console.log('error', error)
-    //   })
-    // // 
-    // this.state.WalItems.forEach(item =>
-    //   axios({
-    //     "method": "GET",
-    //     "url": "https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-lookup-product",
-    //     "headers": {
-    //       "content-type": "application/octet-stream",
-    //       "x-rapidapi-host": "axesso-walmart-data-service.p.rapidapi.com",
-    //       "x-rapidapi-key": "3b8ee98b70mshf74d3fe848bde7dp1f7b3ajsn392bc6e2ea65"
-    //     }, "params": {
-    //       "url": item
-    //     }
-    //   })
-    //     .then((response) => {
-    //       this.setState({ WalProd: [...this.state.WalProd, response.data.productTitle] });
-    //       this.setState({ WalPrice: [...this.state.WalPrice, response.data.price] });
-    //       this.setState({ WalAval: [...this.state.WalAval, response.data.available] });
-    //       // WalProd  = response.data.productTitle;
-    //       // WalPrice = response.data.price;
-    //       // WalAval  = response.data.available;
-    //       // console.log('response', WalProd)
-    //       // console.log('response', WalPrice)
-    //       // console.log('response', WalAval)
-    //     })
-    //     .catch((error) => {
-    //       console.log('error', error)
-    //     })
-    // )
-    /************************************************************************************************** */
-    /***************************************************Kmart */
-    // axios({
-    //   "method":"POST",
-    //   "url":"https://feeditem-kmart.p.rapidapi.com/keyword",
-    //   "headers":{
-    //   "content-type":"application/x-www-form-urlencoded",
-    //   "x-rapidapi-host":"feeditem-kmart.p.rapidapi.com",
-    //   "x-rapidapi-key":"9429490917msh70be5ef070eec0ep1fec84jsnafca7d833618"
-    //   },"data":{
-    //     "keyword": "toilet paper",
-    //   }
-    //   })
-    //   .then((response)=>{
-    //     console.log(response)
-    //   })
-    //   .catch((error)=>{
-    //     console.log(error)
-    //   })
-=======
     // Item[0]="https://www.walmart.com/ip/Cottonelle-FreshCare-Flushable-Wipes-resealable-pack-168-wipes-total/38192980"
     // Item[1]="https://www.walmart.com/ip/Angel-Soft-Toilet-Paper-12-Jumbo-Rolls/713550748"
     // Item[2]="https://www.walmart.com/ip/Charmin-Ultra-Strong-Toilet-Paper-9-Mega-Roll-286-Sheets-Per-Roll/450185839"
@@ -156,9 +77,9 @@ axios({
     // WalProd  = response.data.productTitle;
     // WalPrice = response.data.price;
     // WalAval  = response.data.available;
-    console.log('response', WalProd)
-    console.log('response', WalPrice)
-    console.log('response', WalAval)
+    // console.log('response', WalProd)
+    // console.log('response', WalPrice)
+    // console.log('response', WalAval)
   })
   .catch((error)=>{
     console.log('error', error)
@@ -183,7 +104,6 @@ axios({
 //   .catch((error)=>{
 //     console.log(error)
 //   })
->>>>>>> 5dc8d8366291c2da9989f505d03e86ae85b4e9c6
 
 
     // Kmart api call above

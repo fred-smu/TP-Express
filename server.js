2189215f193f2e6cb1d23841d0f2ef2ec11f9335
Fred Guthrie <fred3@myself.com>
2020-04-07 10:35:19 -0500

    minor change for heroku build

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 // Static directory
 if (process.env.NODE_ENV === "production") {
-app.use(express.static("client/public"));
+app.use(express.static("client/build"));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
